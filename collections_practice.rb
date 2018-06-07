c787cc50ef459f7a0cb931931d76825c8d26e5a0
Marie Cain <mariecain27@gmail.com>
2018-06-07 03:03:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,5 +38,11 @@ def sum_array(integer_array)
 end
 
 def add_s(string_array)
-  
+    string_array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
 end
